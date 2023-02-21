1aa6194734c8d22872031f473f4f1513a3eb146c
vitor barbosa <vitorbarbosa789@gmail.com>
2023-02-21 11:00:53 -0300

    [ADD] modularize

--- api/ImageTransform.py
+++ api/ImageTransform.py
@@ -6,21 +6,34 @@ from PIL import ImageFont, ImageDraw, Image
 import re
 from skimage import io
 
-
-def trataImagem(image_url):
-    img = io.imread(image_url)
+def trataImagem(url):
+    img = io.imread(url)
     rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    return rgb
+    config_tesseract = '--tessdata-dir tessdata --psm 6'
+    resultado = pytesseract.image_to_data(rgb, lang='por', config=config_tesseract, output_type=Output.DICT)
+    min_conf = 60
+    font = './fonts/calibri.ttf'
 
-rgb = trataImagem(image_url)
+    img_copia = rgb.copy()
 
-config_tesseract = '--tessdata-dir tessdata --psm 6'
-resultado = pytesseract.image_to_data(rgb, lang='por', config=config_tesseract, output_type=Output.DICT)
-min_conf = 60
-font = './fonts/calibri.ttf'
+    for i in range(0, len(resultado['text'])):
+        confianca = int(float(resultado['conf'][i]))
 
-def caixa_texto(resultado, img, cor = (255,100,0)):
+        if confianca > min_conf:
+            texto = resultado['text'][i]
+            if not texto.isspace() and len(texto) > 1:
+                x, y, img = caixa_texto(i, resultado, img_copia)
+                img_copia = escreve_texto(resultado, min_conf, texto, x, y, img_copia, font, 12)
+
+    window_name = "image"
+    cv2.imshow(window_name, img_copia)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+# Auxiliares
+def caixa_texto(i, resultado, img, cor = (255,100,0)):
     x = resultado['left'][i]
     y = resultado['top'][i]
     w = resultado['width'][i]
@@ -35,7 +48,7 @@ def cria_documento(file_name, texto):
     arquivo.write(texto + '\n')
     arquivo.close()
 
-def escreve_texto(texto, x, y, img, font, tamanho_texto=32):
+def escreve_texto(resultado, min_conf, texto, x, y, img, font, tamanho_texto=32):
     font = ImageFont.truetype(font, tamanho_texto)
     img_pil = Image.fromarray(img)
     draw = ImageDraw.Draw(img_pil)
@@ -48,20 +61,4 @@ def escreve_texto(texto, x, y, img, font, tamanho_texto=32):
     cria_documento(name_file, texto)
     return img
 
-img_copia = rgb.copy()
 
-for i in range(0, len(resultado['text'])):
-    confianca = int(float(resultado['conf'][i]))
-
-    if confianca > min_conf:
-        texto = resultado['text'][i]
-        if not texto.isspace() and len(texto) > 1:
-            x, y, img = caixa_texto(resultado, img_copia)
-            img_copia = escreve_texto(texto, x, y, img_copia, font, 12)
-
-
-
-window_name = "image"
-cv2.imshow(window_name, img_copia)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
