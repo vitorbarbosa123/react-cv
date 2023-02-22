b7adbe31355d14564f7bba2664dad572c565cabb
vitor barbosa <vitorbarbosa789@gmail.com>
2023-02-22 00:40:15 -0300

    [ADD] sh manjaro

---
+++ install.sh
@@ -0,0 +1,5 @@
+# install in manjaro
+pamac install tesseract
+
+# get tessdata in portuguese language
+wget -O ./api/tessdata/por.traineddata https://github.com/tesseract-ocr/tessdata/blob/main/por.tr
