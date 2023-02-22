0cab29f28424cba41f68855ca32be35952ba2b26
vitor barbosa <vitorbarbosa789@gmail.com>
2023-02-21 22:56:08 -0300

    [ADD] async post + encode64 image

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import { Buffer } from 'buffer';
 
 import './App.css'
 
@@ -7,18 +8,37 @@ function App() {
 
   const [inputValue, setInputValue] = useState('')
   const [imageValue, setImageValue] = useState('')
-
+  const [textValue, setTextValue] = useState('')
+  
   const handleImage = async (event) => {
-    event.preventDefault();
-    try {
-      await axios.post('http://127.0.0.1:5000/images', { input_value: inputValue})
-      .then(response => {
-        setImageValue(response.data);
-      })
-    } catch(error){
-      console.error(error);
+      event.preventDefault()
+      try {
+        await axios.post('http://127.0.0.1:5000/images', { input_value: inputValue })
+        .then(response => {
+          setImageValue(response.config.url);
+        })
+        
+        axios.get('http://127.0.0.1:5000/images' , {
+          headers: {
+            'Accept': 'image/jpeg'
+          },
+          responseType: 'arraybuffer'
+        })
+        .then(response => {
+          const image = new Image()
+          image.src = `data:image/jpeg;base64,${Buffer.from(response.data, 'binary').toString('base64')}`;
+          setImageValue(image.src)
+        })
+
+        axios.get('http://127.0.0.1:5000/text')
+        .then(response => {
+          setTextValue(response.data)
+        })
+      } catch(error){
+        console.error(error);
+      }
     }
-  }
+ 
   const handleChange = (event) => {
     setInputValue(event.target.value)
   }
@@ -38,7 +58,8 @@ function App() {
         <button onClick={handleImage}>Enviar</button>
       </form>
       <div>
-        {imageValue && <img src="http://127.0.0.1:5000/images"/>}
+        {imageValue && <img src={imageValue}/>}
+        {textValue && <p>{textValue}</p>}
       </div>
     </div>
   )
